1f615c9b46025ef7accde75839c4acbde61cd11f
Alexander Davidenko <gypsyman534@gmail.com>
2023-07-18 14:00:21 +0300

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'io-ng-s',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
